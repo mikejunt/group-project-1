72e0a1e8b2b7cb7d26f751f08c0d9f00da43af7a
mikejunt <mikejunt@gmail.com>
2020-01-30 16:15:26 -0600

    testing
    Co-authored-by: Brandon Woita <brwoita@gmail.com>
    Co-authored-by: SJWonder <Shelly.Wonder@outlook.com>
    Co-authored-by: Mike Truax <mike@miketruax.com>

--- rps.js
+++ rps.js
@@ -1,7 +1,15 @@
+// establishes variable
 let gamecounter = 0
 let wincounter = 0
+
+// button selectors
+document.getElementById("rock").addEventListener("click", function () { game(1, 2, 0) });
+document.getElementById("paper").addEventListener("click", function () { game(0, 1, 2) });
+document.getElementById("scissors").addEventListener("click", function () { game(2, 0, 1) });
+
 console.log(gamecounter)
 
+// actual game won/loss/tie logic
 function game(x, y, z) {
     let compchoice = (Math.floor(Math.random() * 3));
     console.log(`Computer chose ${compchoice}.`)
@@ -14,17 +22,57 @@ function game(x, y, z) {
     if (compchoice === draw) { let result = "tie"; tiegame(result) }
 }
 
-
-function wingame(outcome ="") {wincounter++; writegame(); writewin(); newgametext(); resulttext(outcome); console.log(`${wincounter}`) }
-function losegame(outcome ="") { }
-function tiegame(outcome ="") { }
-
-game(0,2,1)
-console.log(`${gamecounter} games`)
-console.log(`${wincounter} wins`)
+// function that executes results of a win
+function wingame(outcome = "") { wincounter++; writegame(); writewin(); newgametext(); resulttext(outcome); console.log("win") }
+// function that executes results of a loss
+function losegame(outcome = "") { writegame(); newgametext(); resulttext(outcome); console.log("loss") }
+// function that executes results of a tie
+function tiegame(outcome = "") { writegame(); newgametext(); resulttext(outcome); console.log("draw") }
 
 
-function writegame() {console.log("Write Game")}
-function writewin() {console.log("write win")}
-function newgametext() {console.log("new game text")}
-function resulttext() {console.log("result text")}
+// function writes games played counter to page
+
+function writegame() { document.getElementById("gamecount").innerText = `${gamecounter}` }
+
+// function writes wins counter to page
+
+function writewin() { document.getElementById('wincount').innerText = `${wincounter}` }
+
+// displays click to play again text
+
+// function newgametext() { document.getElementById(`newgame`).innerText = `${newgame}` }
+
+// { console.log("new game text") }
+
+// displays dynamic win/loss/tie result text
+
+function resulttext(outcome) {
+    let changetext = document.getElementById("results");
+    if (outcome == "win") {
+        console.log("WIN")
+        changetext.innerText = "Congratulations! You won!"
+    }
+    else if (outcome == "loss") {
+        console.log("LOSS")
+        changetext.innerText == "Sorry, you lost."
+    }
+    else {
+        console.log("DRAW")
+        changetext.innerText == "It's a tie, try again."
+    }
+}
+
+
+function newgametext(outcome) {
+    let newgame = document.getElementById("newgame")
+    if (outcome == "win") {
+        newgame.innerText = "Click here to play again!"
+    }
+    if (outcome == "loss") {
+        newgame.innerText = "Click here to play again!"
+    }
+    if (outcome == "tie") {
+        newgame.innerText = "Click here to play again!"
+    }
+
+}
